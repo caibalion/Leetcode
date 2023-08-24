b6d2022fd2834937b50af0f166b81a01450acdb1
caibalion <lucasalexti@gmail.com>
2023-08-24 12:58:58 -0300

    1342 - numberOfSteps #3

--- 1342 - numberOfSteps.js
+++ 1342 - numberOfSteps.js
@@ -1,4 +1,16 @@
 const numberOfSteps = (num) => {
     let value = num;
-
-}
+    let steps = 0;
+    for (let i = 0; value > 0; i++) {
+      if (value % 2 === 0) {
+        value = value / 2;
+        steps++;
+      }
+      if (value % 2 !== 0){
+        value = value - 1;
+        steps++;
+      }
+    }
+    return steps
+  }
+numberOfSteps(14);
